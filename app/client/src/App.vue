50fd491f6ce6958fde234049d5ee5920f3321c46
Bruno <brunogstsho@gmail.com>
2024-03-20 21:36:08 -0300

    install shadcdn

--- app/client/src/App.vue
+++ app/client/src/App.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="bg-zinc-600">
     <h1 class="text-red-500">Home Page</h1>
+    <Button>Click me</Button>
   </div>
 </template>
+
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+</script>
--- app/client/vite.config.ts
+++ app/client/vite.config.ts
@@ -4,6 +4,8 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import VueDevTools from 'vite-plugin-vue-devtools'
+import tailwind from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,5 +17,10 @@ export default defineConfig({
   },
   server: {
     port: 8080
+  },
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()]
+    }
   }
 })
