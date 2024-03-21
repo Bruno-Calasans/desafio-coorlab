b35615c31493ac98e5e34b9f8bdb713211f48653
Bruno <brunogstsho@gmail.com>
2024-03-21 00:51:45 -0300

    create initial form

--- app/client/src/components/layout/MainContent.vue
+++ app/client/src/components/layout/MainContent.vue
@@ -6,6 +6,18 @@
     </header>
 
     <!-- Form -->
-    <div class="p-3 text-black"></div>
+    <div class="text-black flex gap-2 flex-col h-[60%] w-full p-3 shadow-md">
+      <!-- Form Title -->
+      <header
+        class="rounded-t-sm p-3 h-14 bg-slate-900 w-full text-white flex items-center font-bold mb-3"
+      >
+        Calculadora de Viagem
+      </header>
+      <CalcFormTravel />
+    </div>
   </main>
 </template>
+
+<script setup lang="ts">
+import CalcFormTravel from '@/components/CalcTravelForm.vue'
+</script>
