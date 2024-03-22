96fc73744a7175e5e8fe63e93c5ce6518ca49970
Bruno <brunogstsho@gmail.com>
2024-03-21 23:39:39 -0300

    create CitySelector component

---
+++ app/client/src/components/calc-travel-form/CitySelector.vue
@@ -0,0 +1,32 @@
+<template>
+  <Select :modelValue="value" :onUpdate:modelValue="onSelect">
+    <SelectTrigger class="bg-white text-zinc-600">
+      <SelectValue placeholder="Selecione o destino" />
+    </SelectTrigger>
+    <SelectContent class="bg-white text-zinc-600">
+      <SelectItem v-for="city of cities" :value="city" :key="city">{{ city }}</SelectItem>
+    </SelectContent>
+  </Select>
+</template>
+
+<script setup lang="ts">
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue
+} from '@/components/ui/select'
+
+const cities = ['São paulo', 'Fortaleza', 'Belo Horizonte']
+
+defineProps({
+  value: String,
+  onSelect: {
+    type: Function,
+    default(city: string) {
+      return city
+    }
+  }
+})
+</script>
