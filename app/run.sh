8b5e2d7f9419ba5f505181a4fe2548806a0fc42b
Bruno <brunogstsho@gmail.com>
2024-03-23 13:20:33 -0300

    install fastapi and uvicorn

--- app/run.sh
+++ app/run.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
 echo "Implemente aqui o script para executar a sua solução"
+
+# back end
+# sh -ec 'cd api && pipx install poethepoet && poetry install && poe dev'
+# sh -ec 'cd ..'
+# cd api
+# pipx install poethepoet
+# poetry install
+# poe dev
+
+# front-end
+# cd ../client
+# npm install
+# npm run build
