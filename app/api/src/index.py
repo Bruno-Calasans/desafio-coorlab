66b8dc27a62dc7fb92e532f67de1967eee4e3bdc
Bruno <brunogstsho@gmail.com>
2024-03-23 13:51:58 -0300

    create /cities route

--- app/api/src/index.py
+++ app/api/src/index.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
+import json
+
+with open('src/data.json', 'r') as file:
+    travels: list[dict[str, str | int]] = json.load(file).get('transport')
 
 app = FastAPI()
 
 @app.get('/')
 def home():
     return {'message': 'hello'}
+
+@app.get('/cities')
+def getCities():
+    cities: set[str] = set()
+
+    for travel in travels:
+        if 'city' in travel:
+            cities.add(travel.get('city'))
+
+    return cities
+        
